ce9dad6896f7a6eaee5c1a26c31ea10eb6a243d7
JulyaDon <julya.p.don@gmail.com>
2016-08-08 14:25:06 +0300

    Initial commit

--- DynamicDLLConsoleCPP/main.cpp
+++ DynamicDLLConsoleCPP/main.cpp
@@ -5,21 +5,25 @@
 
 using namespace std;
 
-typedef char* (*PFN_SayHello)();
+//typedef char* (*PFN_SayHello)();
+typedef char* (*PFN_SayHello)(char*, int k);
 
 int main()
 {
 	HINSTANCE hDynamicDLL;
 
-	if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	//if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	if ((hDynamicDLL = LoadLibrary(TEXT("CipherTritemiusDLL"))) == NULL)
 	{
 		
 	}
 	else
 	{
 		PFN_SayHello pfn_SayHello;
-		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
-		cout<<(*pfn_SayHello)()<<endl;
+		//pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
+		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "mainTritemius");
+		// << (*pfn_SayHello)() << endl;
+		cout<<(*pfn_SayHello)("hello", 3)<<endl;
 	}
 
 
